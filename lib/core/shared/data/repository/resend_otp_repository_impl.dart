21f81109410c1cd7e80e3b6879c1544453bf1517
Ahmed Yahia <ahmed22y10@gmail.com>
2026-03-18 16:59:10 +0200

    feat(register): Implement user registration flow using Cubit and refactor resend OTP to use domain entities.

--- lib/core/shared/data/repository/resend_otp_repository_impl.dart
+++ lib/core/shared/data/repository/resend_otp_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:explaino/config/base_response/base_response.dart';
 import 'package:explaino/core/shared/data/data_sources/remote/resend_otp_remote_data_source.dart';
-import 'package:explaino/core/shared/data/models/otp/resend_otp_request_model/resend_otp_request_model.dart';
+import 'package:explaino/core/shared/data/mappers/otp/resend_otp_request_mapper.dart';
+import 'package:explaino/core/shared/domain/entities/otp/resend_otp_request_entity.dart';
 import 'package:explaino/core/shared/domain/repository/resend_otp_repository.dart';
 import 'package:injectable/injectable.dart';
 
@@ -9,8 +10,10 @@ class ResendOtpRepositoryImpl implements ResendOtpRepository {
   final ResendOtpRemoteDataSource _resendOtpRemoteDataSource;
   ResendOtpRepositoryImpl(this._resendOtpRemoteDataSource);
   @override
-  Future<BaseResponse<String>> resendOtp(ResendOtpRequestModel request) async {
-    final response = await _resendOtpRemoteDataSource.resendOtp(request);
+  Future<BaseResponse<String>> resendOtp(ResendOtpRequestEntity request) async {
+    final response = await _resendOtpRemoteDataSource.resendOtp(
+      request.toModel(),
+    );
     return response.when(
       success: (data) {
         return BaseResponse<String>.success(data.message);
--- lib/core/shared/domain/repository/resend_otp_repository.dart
+++ lib/core/shared/domain/repository/resend_otp_repository.dart
@@ -1,6 +1,6 @@
 import 'package:explaino/config/base_response/base_response.dart';
-import 'package:explaino/core/shared/data/models/otp/resend_otp_request_model/resend_otp_request_model.dart';
+import 'package:explaino/core/shared/domain/entities/otp/resend_otp_request_entity.dart';
 
 abstract interface class ResendOtpRepository {
-  Future<BaseResponse<String>> resendOtp(ResendOtpRequestModel request);
+  Future<BaseResponse<String>> resendOtp(ResendOtpRequestEntity request);
 }
