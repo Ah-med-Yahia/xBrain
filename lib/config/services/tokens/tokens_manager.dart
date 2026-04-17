4cafbd18d86e6b54af3cc7949058dc78b0bc0cdf
Ahmed Yahia <ahmed22y10@gmail.com>
2026-04-17 21:59:49 +0200

    refactor(register): Update TokensManager dependency injection and sync project dependencies

--- lib/config/services/tokens/tokens_manager.dart
+++ lib/config/services/tokens/tokens_manager.dart
@@ -4,7 +4,7 @@ import 'package:explaino/core/constants/api_constants.dart';
 import 'package:explaino/core/shared/data/models/refresh_token_response_model/refresh_token_response_model.dart';
 import 'package:injectable/injectable.dart';
 
-@lazySingleton
+@LazySingleton(as: TokensManagerContract)
 class TokensManager implements TokensManagerContract {
   final Dio dio;
   TokensManager(this.dio);
