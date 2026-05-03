556f217eb36d996e24948437bb82fc22242cc7f7
Ahmed Yahia <ahmed22y10@gmail.com>
2026-05-03 09:31:10 +0300

    feat(app_icon): Add devtools options and integrate BLoC and Dio dependencies

--- lib/features/tabs/profile/edit_profile/api/api_clients/edit_profile_api_client.dart
+++ lib/features/tabs/profile/edit_profile/api/api_clients/edit_profile_api_client.dart
@@ -3,7 +3,7 @@ import 'package:explaino/core/constants/api_constants.dart';
 import 'package:explaino/core/shared/data/models/auth/user_model/user_model.dart';
 import 'package:explaino/features/tabs/profile/edit_profile/data/models/request/edit_profile_request_model.dart';
 import 'package:injectable/injectable.dart';
-import 'package:retrofit/http.dart';
+import 'package:retrofit/retrofit.dart';
 
 part 'edit_profile_api_client.g.dart';
 
