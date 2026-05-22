701c550bfd9b63678a29da2589e92d7b1b053b64
Ahmed Yahia <ahmed22y10@gmail.com>
2026-05-22 21:13:02 +0300

    feat(add_feat): Implement add question and add post domain and data layers

---
+++ lib/features/tabs/add_question_or_posts/data/repositories/add_question_or_posts_repo_impl.dart
@@ -0,0 +1,43 @@
+import 'package:explaino/config/base_response/base_response.dart';
+import 'package:explaino/features/tabs/add_question_or_posts/data/data_sources/remote/remote_add_question_or_posts_data_source.dart';
+import 'package:explaino/features/tabs/add_question_or_posts/data/mappers/add_post_request_mapper.dart';
+import 'package:explaino/features/tabs/add_question_or_posts/data/mappers/add_question_request_mapper.dart';
+import 'package:explaino/features/tabs/add_question_or_posts/domain/entities/request/add_post_request_entity.dart';
+import 'package:explaino/features/tabs/add_question_or_posts/domain/entities/request/add_question_request_entity.dart';
+import 'package:explaino/features/tabs/add_question_or_posts/domain/repositories/add_question_or_posts_repo.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: AddQuestionOrPostsRepo)
+class AddQuestionOrPostsRepoImpl implements AddQuestionOrPostsRepo {
+  final RemoteAddQuestionOrPostsDataSource _remoteDataSource;
+
+  AddQuestionOrPostsRepoImpl(this._remoteDataSource);
+
+  @override
+  Future<BaseResponse<void>> addQuestion(
+    AddQuestionRequestEntity request,
+  ) async {
+    final result = await _remoteDataSource.addQuestion(request.toModel());
+    return result.when(
+      success: (_) {
+        return const BaseResponse<void>.success(null);
+      },
+      failure: (error) {
+        return BaseResponse<void>.failure(error);
+      },
+    );
+  }
+
+  @override
+  Future<BaseResponse<void>> addPost(AddPostRequestEntity request) async {
+    final result = await _remoteDataSource.addPost(request.toModel());
+    return result.when(
+      success: (_) {
+        return const BaseResponse<void>.success(null);
+      },
+      failure: (error) {
+        return BaseResponse<void>.failure(error);
+      },
+    );
+  }
+}
